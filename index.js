74bf982463318174b522940a8fe11bfb471f280a
Ganesh Poudel <poudelga@gmail.com>
2019-11-07 13:23:34 +0200

    express app, get /and get /demo

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 'use strict';
-console.log('Hello');
+const express = require('express');
+const app = express()
 
+app.get('/', (req, res) =>{
+    res.send('Hello from my Node server');
+});
+
+app.get('/demo', (req, res) =>{
+    res.send('demo');
+});
+
+app.listen(3000);
