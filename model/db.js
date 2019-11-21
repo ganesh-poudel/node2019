d8b1cae8c6d67a0a7ba5760fdb1657f4eb76e0c9
Ganesh Poudel <poudelga@gmail.com>
2019-11-21 16:02:00 +0200

    making secure http to https

--- model/db.js
+++ model/db.js
@@ -8,8 +8,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_Name
-   
+    database: process.env.DB_NAME   
   });
 
   module.exports = connection.promise();
